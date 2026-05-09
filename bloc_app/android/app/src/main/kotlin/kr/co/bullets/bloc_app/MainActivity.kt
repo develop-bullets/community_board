c1c77901cdac558a1863f67714290e916eb44b16
bullets-jiyoungryu <jy.ryu.jiyoung@gmail.com>
2026-05-09 14:34:33 +0900

    Initial commit

---
+++ bloc_app/android/app/src/main/kotlin/kr/co/bullets/bloc_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package kr.co.bullets.bloc_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ bloc_app/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/di/di.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  configureDependencies();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Community Board Bloc',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Project Setup Complete!'),
+        ),
+      ),
+    );
+  }
+}
