f6bb3690892858c325eed262d612773d820495b1
bullets-jiyoungryu <jy.ryu.jiyoung@gmail.com>
2026-05-10 17:47:27 +0900

    presentation: injectable

--- bloc_app/lib/core/di/register_module.dart
+++ bloc_app/lib/core/di/register_module.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 
-// import 'package:data_supabase/auth.dart';
+import 'package:data_supabase/auth.dart';
 // import 'package:data_supabase/post.dart';
 // import 'package:data_supabase/profile.dart';
 // import 'package:data_supabase/search.dart';
-// import 'package:domain/auth.dart';
+import 'package:domain/auth.dart';
 // import 'package:domain/post.dart';
 // import 'package:domain/profile.dart';
 // import 'package:domain/search.dart';
@@ -19,21 +19,30 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 //   instance.dispose();
 // }
 
+// abstract getter 선언만 해도 에러가 발생하지 않은 이유는 RegisterModule이 abstract class이기 때문입니다.
+// 그렇지만 local package라 하더라도 복잡한 초기화 로직을 가지고 있을 경우에는
+// 단순 getter 선언만으로는 안되고 구체적으로 인스턴스를 얻는 방법을 알려줘야 합니다.
 @module
 abstract class RegisterModule {
+  // SupabaseClient는 외부 라이브러리 클래스 입니다.
+  // injectable은 이 클래스의 인스턴스를 어떻게 얻어야 하는지 알 수 없습니다.
+  // 왜냐하면 단순 생성자 호출이 아니라 superbase.instance.client를 통해 얻어야 하기 때문입니다.
+  // 단순 생성자 호출로 인스턴스를 얻을 수 있다면 get 선언만으로도 충분합니다.
+  // 그래서 우리는 이 클래스가 필요하면 이 코드를 실행해서 인스턴스를 가져와라고 직접 방법을 알려줘야 합니다.
   @singleton
   SupabaseClient get supabaseClient => Supabase.instance.client;
 
   // @singleton
   // GoRouter router(AuthenticationBloc authBloc) => createRouter(authBloc);
 
-  // --- Data Layer Registration (LazySingleton) ---
+  /// --- Data Layer Registration (LazySingleton) ---
   // auth
-  // @LazySingleton(as: AuthRemoteDataSource)
-  // SupabaseAuthRemoteDataSource get authRemoteDataSource;
+  // @LazySingleton으로 애노테이트 한 이유는 필요할 때, 인스턴스를 생성하면 되기 때문입니다.
+  @LazySingleton(as: AuthRemoteDataSource)
+  SupabaseAuthRemoteDataSource get authRemoteDataSource;
 
-  // @LazySingleton(as: AuthRepository)
-  // AuthRepositoryImpl get authRepository;
+  @LazySingleton(as: AuthRepository)
+  AuthRepositoryImpl get authRepository;
 
   // post
   // @LazySingleton(as: PostRemoteDataSource)
@@ -65,16 +74,20 @@ abstract class RegisterModule {
   // @LazySingleton(as: SearchRepository)
   // SearchRepositoryImpl get searchRepository;
 
-  // --- Domain Layer (UseCases) Registration (Injectable - factory) ---
+  /// --- Domain Layer (UseCases) Registration (Injectable - factory) ---
+  // UseCases는 호출 시점에만 인스턴스를 만들고 바로 사용하면 되므로 팩토리 방식
+  // 즉 injectable로 등록하는게 적합합니다.
+  // 이렇게 하면 앱 전체 단 하나의 인스턴스만 유지하는 싱글톤과 달리
+  // 요청이 있을 때마다 새로운 객체를 만들어 사용하게 됩니다.
   // auth
-  // @injectable
-  // SignupUseCase get signupUseCase;
+  @injectable
+  SignupUseCase get signupUseCase;
 
-  // @injectable
-  // LoginUseCase get loginUseCase;
+  @injectable
+  LoginUseCase get loginUseCase;
 
-  // @injectable
-  // LogoutUseCase get logoutUseCase;
+  @injectable
+  LogoutUseCase get logoutUseCase;
 
   // post
   // @injectable
