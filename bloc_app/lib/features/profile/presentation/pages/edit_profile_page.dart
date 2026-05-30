6cb716f693c95c4a2cbd1dec1900081369359360
bullets-jiyoungryu <jy.ryu.jiyoung@gmail.com>
2026-05-30 17:48:18 +0900

    presentation: 모든 페이지들의 templates

---
+++ bloc_app/lib/features/profile/presentation/pages/edit_profile_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const EditProfileView();
+  }
+}
+
+class EditProfileView extends StatelessWidget {
+  const EditProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Edit Profile Page View'),
+      ),
+    );
+  }
+}
